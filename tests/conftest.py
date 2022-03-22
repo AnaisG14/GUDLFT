dabea4755b868bc28dd34979191f0df404b87465
AnaisG14 <gatard.anais@gmail.com>
2022-03-22 23:12:36 +0100

    add fixture in conftest in order to mock contantes : clubs et competitions

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,3 +7,37 @@ def client():
     server.app.config.update({"TESTING": True})
     with server.app.test_client() as client:
         yield client
+
+
+@pytest.fixture
+def data_club_json():
+    data = [
+        {
+            "name": "Club 1",
+            "email": "club1@test.com",
+            "points": "1"
+        },
+        {
+            "name": "Club 2",
+            "email": "club2@test.com",
+            "points": "2"
+        }
+    ]
+    return data
+
+
+@pytest.fixture
+def data_competition_json():
+    data = [
+        {
+            "name": "Competition 1",
+            "date": "2022-03-20 10:00:00",
+            "numberOfPlaces": "25"
+        },
+        {
+            "name": "Competition 2",
+            "date": "2022-05-22 10:00:00",
+            "numberOfPlaces": "13"
+        }
+    ]
+    return data
--- tests/test_request.py
+++ tests/test_request.py
@@ -1,4 +1,5 @@
-from tests.conftest import client
+from tests.conftest import client, data_club_json, data_competition_json
+import server
 
 
 def test_status_code_request_200(client):
@@ -12,14 +13,18 @@ def test_request_display_example(client):
     assert "Welcome to the GUDLFT Registration Portal!" in data
 
 
-def test_login_with_email_valid(client):
-    email = "john@simplylift.co"
+def test_login_with_email_valid(client, mocker, data_club_json):
+    email = "club1@test.com"
+    mocker.patch.object(server, 'clubs', data_club_json)
     test_log = client.post('/showSummary', data={'email': email})
+    data = test_log.data.decode()
+    assert "Points available" in data
     assert test_log.status_code == 200
 
 
-def test_user_email_unknown(client):
+def test_user_email_unknown(client, mocker, data_club_json):
     email = "no_register_email@test.fr"
+    mocker.patch.object(server, 'clubs', data_club_json)
     test_log = client.post('/showSummary', data={'email': email}, follow_redirects=True)
     data = test_log.data.decode()
     assert "Welcome to the GUDLFT Registration Portal!" in data
