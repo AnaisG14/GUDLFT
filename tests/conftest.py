3784fff184b31d81f27df15baf626d730581b927
AnaisG14 <gatard.anais@gmail.com>
2022-03-25 19:26:44 +0100

    modification of test_purchase_places.py with a class_test instead of a function

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 import server
 
 
-@pytest.fixture
+@pytest.fixture()
 def client():
     server.app.config.update({"TESTING": True})
     with server.app.test_client() as client:
--- tests/test_purchase_places.py
+++ tests/test_purchase_places.py
@@ -1,11 +1,16 @@
 from tests.conftest import client, mock_clubs, mock_competitions
 from server import purchase_places
+import server
 
 
-def test_updating_points_after_purchase_places(client, mock_clubs, mock_competitions):
-    request = client.post('/purchasePlaces', data={'places': '4', 'competition': 'Competition 1', 'club': 'Club 1'})
-    data = request.data.decode()
-    # mocks_club1 has 10 points, it must have 6 after the purchase
-    assert 'Points available: 6' in data
+class TestPurchasePlaces:
+    booking_places = {'places': '4', 'competition': 'Competition 1', 'club': 'Club 1'}
+
+    def test_updating_points_after_purchase_places(self, client, mock_clubs, mock_competitions):
+        request = client.post('/purchasePlaces', data=self.booking_places)
+        data = request.data.decode()
+        # mocks_club1 has 10 points, it must have 6 after the purchase
+        assert 'Points available: 6' in data
+
 
 
