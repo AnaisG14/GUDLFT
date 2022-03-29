6e84d085c94a75d76944b86806fe17501c887c47
AnaisG14 <gatard.anais@gmail.com>
2022-03-29 21:58:26 +0200

    add a private method in order to reduce repeated code in public test method

--- tests/test_purchase_places.py
+++ tests/test_purchase_places.py
@@ -4,30 +4,38 @@ import server
 
 
 class TestPurchasePlaces:
-    booking_places = {'places': '4', 'competition': 'Competition 1', 'club': 'Club 1'}
+    booking_places = {'places': '4', 'competition': 'Competition 2', 'club': 'Club 1'}
+
+    @staticmethod
+    def _test_request(request, captured_templates):
+        request = request
+        assert request.status_code == 200
+        assert len(captured_templates) == 1
+        template, context = captured_templates[0]
+        return template, context
 
     def test_updating_points_after_purchase_places(self, client, captured_templates, mock_clubs, mock_competitions):
         request = client.post('/purchasePlaces', data=self.booking_places)
         # mocks_club1 has 10 points, it must have 6 after the purchase
-        assert request.status_code == 200
-        assert len(captured_templates) == 1
-        template, context = captured_templates[0]
+        template, context = self._test_request(request, captured_templates)
         assert template.name == "welcome.html"
         assert context["club"]['points'] == 6
 
     def test_booking_places_in_a_competition(self, client, captured_templates, mock_clubs, mock_competitions):
         request = client.get('/book/Competition 2/Club 2')
-        assert request.status_code == 200
-        assert len(captured_templates) == 1
-        template, context = captured_templates[0]
+        template, context = self._test_request(request, captured_templates)
         assert template.name == "booking.html"
 
     def test_not_allowed_booking_places_in_past_competition(self, client, captured_templates, mock_clubs, mock_competitions):
         request = client.get('/book/Competition 1/Club 1')
-        assert request.status_code == 200
-        assert len(captured_templates) == 1
-        template, context = captured_templates[0]
+        template, context = self._test_request(request, captured_templates)
         assert template.name == "welcome.html"
 
+    def test_booking_places(self, client, captured_templates, mock_clubs, mock_competitions):
+        request = client.post('/purchasePlaces', data=self.booking_places)
+        template, context = self._test_request(request, captured_templates)
+        assert template.name == "welcome.html"
+        data = request.data.decode()
+        assert 'Great-booking complete' in data
 
 
