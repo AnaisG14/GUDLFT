66c3d86225ed560b0673ad198ea04cd537a78235
AnaisG14 <gatard.anais@gmail.com>
2022-04-02 10:15:49 +0200

    delete an integration test

--- tests/integration_tests/test_booking_places_using_transform_string_in_datetime.py
+++
@@ -1,7 +0,0 @@
-def test_not_allowed_booking_places_in_past_competition(client, captured_templates, mock_clubs,
-                                                        mock_competitions):
-    request = client.get('/book/Competition 1/Club 1')
-    assert request.status_code == 200
-    assert len(captured_templates) == 1
-    template, context = captured_templates[0]
-    assert template.name == "welcome.html"
